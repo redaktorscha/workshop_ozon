fca7359af8d475b15a13b6f4427780235a57b4cb
redaktorscha <39005857+redaktorscha@users.noreply.github.com>
2019-08-15 03:37:21 +0300

    toggle checkbox togglecart addtocart

--- src/index.js
+++ src/index.js
@@ -4,53 +4,115 @@ const cartWrapper = document.querySelector('.cart-wrapper');
 const cartEmpty = document.querySelector('#cart-empty');
 const goodsCounter = document.querySelector('.counter');
 
-//корзина
-const btnCart = document.getElementById('cart');
-const modalCart = document.querySelector('.cart');
-const closeBtn = document.querySelector('.cart-close');
-const body = document.body;
 
-//открытие корзины в модальном окне
-btnCart.addEventListener('click', () => {
-    modalCart.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
 
-//закрытие модального окна с корзиной
-closeBtn.addEventListener('click', () => {
-    modalCart.style.display = 'none';
-    body.style.overflow = '';
-});
+//открытие-закрытие корзины
+const toggleCart = (btnCart, modalCart, closeBtn) => {
+
+    btnCart.addEventListener('click', () => {
+        modalCart.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    });
+
+
+    closeBtn.addEventListener('click', () => {
+        modalCart.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+}
 
 
 //счетчик товаров в корзине
-const showData = () => {
+const showCartData = () => {
+    const cardsPrice = cartWrapper.querySelectorAll('.card-price');
+    const cartTotal = document.querySelector('.cart-total span');
     const shoppedGoods = cartWrapper.querySelectorAll('.card');
     goodsCounter.textContent = shoppedGoods.length;
+
+    let sum = 0;
+    cardsPrice.forEach((cardPrice) => {
+        let price = parseFloat(cardPrice.textContent);
+        sum += price;
+    });
+
+    cartTotal.textContent = sum;
+
+    if (shoppedGoods.length === 0) {
+        cartWrapper.appendChild(cartEmpty);
+    } else {
+        cartEmpty.remove();
+    }
+
+
+
 };
 
-//добавление товаров в корзину
-cards.forEach((card) => {
-    const btn = card.querySelector('button');
+//добавление и удаление товаров из корзины
+const addToCart = () => {
+    const addedCards = document.querySelectorAll('.goods .card');
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    const cartEmpty = document.querySelector('#cart-empty');
+    const goodsCounter = document.querySelector('.counter');
 
-    btn.addEventListener('click', () => {
-        const cardClone = card.cloneNode(true);
-        cartWrapper.appendChild(cardClone);
-        cartEmpty.remove();
-        showData();
+    addedCards.forEach((card) => {
+        const btn = card.querySelector('button');
+    
+        btn.addEventListener('click', () => {
+            const cardClone = card.cloneNode(true);
+            cartWrapper.appendChild(cardClone);
+            cartEmpty.remove();
+            showCartData();
+    
+            const removeBtn = cardClone.querySelector('.btn'); //изменение текста кнопки в добавленной карточке
+            removeBtn.textContent = 'Удалить из корзины';
+            removeBtn.addEventListener('click', () => {
+                cardClone.remove();
+                showCartData();
+            });
+        });
     });
-});
+}
+
 
 
 //анимация чекбоксов
-const checkbox = document.querySelectorAll('.filter-check_checkbox');
-
-checkbox.forEach((elem) => {
-    elem.addEventListener('change', function () {
-        if (this.checked) {
-            this.nextElementSibling.classList.add('checked')
-        } else {
-            this.nextElementSibling.classList.remove('checked')
-        }
+const toggleCheckbox = (checkbox) => {
+    checkbox.forEach((elem) => {
+        elem.addEventListener('change', function () {
+            if (this.checked) {
+                console.log('checked');
+                this.nextElementSibling.classList.add('checked');
+            } else {
+                this.nextElementSibling.classList.remove('checked');
+            }
+        });
     });
-});
+};
+
+//фильтр акции
+const actionFilter = () => {
+    const cards = document.querySelectorAll('.goods .card'),
+        discountCheckbox = document.getElementById('discount-checkbox'),
+        min = document.getElementById('min'),
+        max = document.getElementById('max');
+
+        discountCheckbox.addEventListener('click', () => {
+            cards.forEach((card) => {
+                if (discountCheckbox.checked) {
+                    if (!card.querySelector('.card-sale')) {
+                        card.parentNode.style.display = 'none';
+                    }
+                    card.parentNode.style.display = '';
+                }
+                }
+                
+
+            )
+        })
+
+}
+
+toggleCheckbox(document.querySelectorAll('.filter-check_checkbox'));
+toggleCart(document.getElementById('cart'), document.querySelector('.cart'), document.querySelector('.cart-close'));
+addToCart();
+actionFilter();
